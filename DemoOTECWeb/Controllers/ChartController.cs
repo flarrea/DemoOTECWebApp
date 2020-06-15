796b7174cd33b1e6b49ac45957a797962e680467
flarrea <flarreas@gmail.com>
2020-06-14 20:13:08 -0400

    #5 DemoOTECWeb Se agrega gráficos de alumnos por curso

---
+++ DemoOTECWeb/Controllers/ChartController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoOTECWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoOTECWeb.Controllers
+{
+    public class ChartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetChart()
+        {
+            var data = _context.Cursos.ToList();
+            return Json(data);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
